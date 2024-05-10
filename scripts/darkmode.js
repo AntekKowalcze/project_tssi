382ac6c6c1359ff54c3b5d791604b2c570e932b2
AntekKowalcze <antekkowalcze8@gmail.com>
2024-05-10 17:48:04 +0200

    formatted files and directed every feature code to distinct files

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -6,53 +6,51 @@ const listItem = document.querySelectorAll(".header-nav-item");
 const listLink = document.querySelectorAll(".header-nav-link");
 // swaping prism (code style sheet)
 function swapStyleSheet(sheet) {
-  document.getElementById("sheet").setAttribute("href", sheet);  
+  document.getElementById("sheet").setAttribute("href", sheet);
 }
-// function which enables darkmode 
+// function which enables darkmode
 const enableDarkMode = () => {
-// for each list item chage class to darkmode-list
-listItem.forEach((item) =>{
-  item.classList.add("darkmode-list");
-})
+  // for each list item chage class to darkmode-list
+  listItem.forEach((item) => {
+    item.classList.add("darkmode-list");
+  });
   // for each link in list delete class darkmode-list
-listLink.forEach((link) => {
-  link.classList.add("color-toogle");
-})
-// change colors and fonts 
-document.body.classList.add("darkmode");
-// swap stylesheet for prism
-swapStyleSheet("../CSS/prism_dark_mode.css");
-// change state of local storage
-localStorage.setItem('state', 'enabled');
-
-}
+  listLink.forEach((link) => {
+    link.classList.add("color-toogle");
+  });
+  // change colors and fonts
+  document.body.classList.add("darkmode");
+  // swap stylesheet for prism
+  swapStyleSheet("../CSS/prism_dark_mode.css");
+  // change state of local storage
+  localStorage.setItem("state", "enabled");
+};
 const disableDarkMode = () => {
-// for each list item delete class darkmode-list
-  listItem.forEach((item) =>{
+  // for each list item delete class darkmode-list
+  listItem.forEach((item) => {
     item.classList.remove("darkmode-list");
-  })
+  });
   // for each link in list delete class darkmode-list
   listLink.forEach((link) => {
     link.classList.remove("color-toogle");
-  })
-  // change colors and fonts to normal 
-  document.body.classList.remove("darkmode")
+  });
+  // change colors and fonts to normal
+  document.body.classList.remove("darkmode");
   // swap stylesheet for prism
   swapStyleSheet("../CSS/prism_light_mode.css");
-// change state of local storage
-  localStorage.setItem('state', 'disabled');
-
-  }
-  // checking if darkmode was enabled last time site was visited
-  if  (state == "enabled"){
-    enableDarkMode();
-    // changing orientation off button to make actual state always on top
-    toggleButton.classList.add("dark");
-  } 
+  // change state of local storage
+  localStorage.setItem("state", "disabled");
+};
+// checking if darkmode was enabled last time site was visited
+if (state == "enabled") {
+  enableDarkMode();
+  // changing orientation off button to make actual state always on top
+  toggleButton.classList.add("dark");
+}
 toggleButton.addEventListener("click", () => {
   // get state from local storage and enable or disable darkmode
-  state = localStorage.getItem("state")
-  if (state!='enabled'){
+  state = localStorage.getItem("state");
+  if (state != "enabled") {
     toggleButton.classList.add("dark");
     enableDarkMode();
   } else {
@@ -61,7 +59,6 @@ toggleButton.addEventListener("click", () => {
     toggleButton.classList.add("light");
     setTimeout(() => {
       toggleButton.classList.remove("light");
-    },600)
+    }, 600);
   }
-})
-
+});
